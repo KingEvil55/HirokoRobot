0a8b58d27e74a34ec72071b67b72e04878bcdae2
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-10-17 13:34:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_USERNAME = getenv("BOT_USERNAME", "HirokoRobot")
+API_ID = int(getenv("API_ID", "27367626"))
+API_HASH = getenv("API_HASH", "a291c4806cab38bc22b27c12ed456413")
+BOT_USERNAME = getenv("BOT_USERNAME", "MrSatoruGojo_Bot")
 COMMAND_HANDLER = ["/", "!"]
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "6280048819"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819").split()))
+OWNER_ID = int(getenv("OWNER_ID", "1138802391"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Hiroko:Hiroko@cluster0.1hztkgz.mongodb.net/?retryWrites=true&w=majority")
 SESSION_STRING = getenv("SESSION_STRING", "")
